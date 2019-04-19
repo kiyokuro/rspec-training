df9e7d3e785617469e1939bbebd8ae326148fa5a
kiyokuro <kuro6412@gmail.com>
2019-04-19 19:18:23 +0900

    slackに通知できる
    コメント追加

--- lib/weather_bot.rb
+++ lib/weather_bot.rb
@@ -1,2 +1,17 @@
+require 'slack-notifier'
+require 'yaml'
+require 'pry'
+
 class WeatherBot
+  # /config/slack.ymlを作ってwebhock_urlをキーにしてurlを追加する
+  # webhock_url: ***
+  WEBHOCK_URL = YAML.load_file('config/slack.yml')['webhock_url']
+
+  def notify_forecast
+    slack_client.ping('明日は晴れです☀️')
+  end
+
+  def slack_client
+    Slack::Notifier.new(WEBHOCK_URL)
+  end
 end
