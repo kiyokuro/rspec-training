292607c5dd9fcb01bf2d91cd58d196819f0aa011
kiyokuro <kuro6412@gmail.com>
2019-04-24 14:23:11 +0900

    mockを使ってslack通知が飛ばないように変更

--- spec/weather_bot_spec.rb
+++ spec/weather_bot_spec.rb
@@ -3,8 +3,14 @@ require 'spec_helper'
 RSpec.describe WeatherBot do
   describe '#notify_forecast' do
     let(:weather_bot) { WeatherBot.new }
+    let(:slack_client_mock) { double('Slack clinet') }
+
+    before do
+      allow(weather_bot).to receive(:slack_client).and_return(slack_client_mock)
+    end
 
     it 'エラーなく予報を通知すること' do
+      expect(slack_client_mock).to receive(:ping)
       expect { weather_bot.notify_forecast }.not_to raise_error
     end
   end
